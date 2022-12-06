704aa16038c2e30d06c04c8497be8ad459ba6c3b
Faisal H Almuhaidly <40997682+smike7@users.noreply.github.com>
2022-12-06 14:41:48 +0300

    Created first screen

---
+++ onboardngDemo/firstVC.swift
@@ -0,0 +1,119 @@
+//
+//  ViewController.swift
+//  onboardngDemo
+//
+//  Created by Faisal on 05/12/2022.
+//
+
+import UIKit
+import SnapKit
+
+class firstVC: UIViewController {
+    
+    private lazy var largeTitleLbl: UILabel = {
+        let lbl = UILabel()
+        lbl.textColor = .label
+        lbl.font = UIFontMetrics.default.scaledFont(for: UIFont.systemFont(ofSize: 40, weight: .bold), compatibleWith: .current)
+        lbl.textAlignment = .left
+        lbl.lineBreakMode = .byWordWrapping
+        lbl.numberOfLines = 0
+        lbl.text = "Ready to get started ?"
+        return lbl
+    }()
+    
+    private lazy var subTitleLbl: UILabel = {
+        let lbl = UILabel()
+        lbl.textColor = .secondaryLabel
+        lbl.font = UIFontMetrics.default.scaledFont(for: UIFont.systemFont(ofSize: 18, weight: .regular), compatibleWith: .current)
+        lbl.textAlignment = .left
+        lbl.lineBreakMode = .byWordWrapping
+        lbl.numberOfLines = 0
+        lbl.text = "Explore, sell and create your NFTs"
+        return lbl
+    }()
+    
+    private lazy var startBtn: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setTitle("Start", for: .normal)
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        btn.setTitleColor(.black, for: .normal)
+        btn.backgroundColor = .white
+        btn.layer.cornerRadius = 16
+        btn.layer.cornerCurve = .continuous
+        btn.layer.shadowRadius = 15
+        btn.layer.shadowOffset = CGSize(width: 0, height: 20)
+        btn.layer.shadowOpacity = 0.35
+        btn.layer.shadowColor = UIColor.white.cgColor
+        btn.layer.masksToBounds = false
+        return btn
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        overrideUserInterfaceStyle = .dark
+        setupLayout()
+        startBtn.addTarget(self, action: #selector(startTapped), for: .touchUpInside)
+    }
+
+    @objc private func startTapped() {
+        print("Tapped")
+    }
+}
+//MARK: - Layout Constraints
+extension firstVC {
+    func setupLayout() {
+        view.addSubview(largeTitleLbl)
+        largeTitleLbl.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.top)
+                .offset(150)
+            make.left.right.equalTo(view)
+                .offset(20)
+        }
+        
+        view.addSubview(subTitleLbl)
+        subTitleLbl.snp.makeConstraints { make in
+            make.top.equalTo(largeTitleLbl.snp.bottom)
+                .offset(12)
+            make.left.right.equalTo(view)
+                .offset(20)
+        }
+        let sv = UIStackView()
+        sv.axis = .horizontal
+        sv.distribution = .fillEqually
+        sv.alignment = .center
+        sv.spacing = 10
+        view.addSubview(sv)
+        
+        sv.snp.makeConstraints { make in
+            make.top.equalTo(subTitleLbl.snp.bottom)
+                .offset(40)
+            make.right.equalTo(view)
+                .offset(-20)
+            make.left.equalTo(view)
+                .offset(20)
+            make.height.equalTo(166)
+        }
+        for imageName in ["image1", "image2", "image3"] {
+            let imageV = UIImageView()
+            guard let img = UIImage(named: imageName) else {continue}
+            imageV.image = img
+            imageV.contentMode = .scaleToFill
+            sv.addArrangedSubview(imageV)
+        }
+        
+        view.addSubview(startBtn)
+        startBtn.snp.makeConstraints { make in
+            make.bottom.equalTo(view.snp.bottom)
+                .offset(-150)
+            make.height.equalTo(45)
+            make.right.equalTo(view)
+                .offset(-20)
+            make.left.equalTo(view)
+                .offset(20)
+        }
+        
+        
+    }
+    
+    
+}
+
